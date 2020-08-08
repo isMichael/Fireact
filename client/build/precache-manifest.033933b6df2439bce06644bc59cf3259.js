d883cb5a3ccab1c5512786cd96ea905eb68d619d
Michael A. Cruz <mcruz@mail.sib.org.bz>
2020-08-08 12:40:51 -0600

    latest

---
+++ client/build/precache-manifest.033933b6df2439bce06644bc59cf3259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70558f751fc11080362736b4093eff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc62249c32fc0b2826e2",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "67e4a479d1065f693db0",
+    "url": "/static/css/main.0aaaf005.chunk.css"
+  },
+  {
+    "revision": "dc62249c32fc0b2826e2",
+    "url": "/static/js/2.43482eb9.chunk.js"
+  },
+  {
+    "revision": "f1b8bfc3915e9dbfa89fdc50283a6b25",
+    "url": "/static/js/2.43482eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e4a479d1065f693db0",
+    "url": "/static/js/main.7090f086.chunk.js"
+  },
+  {
+    "revision": "5fad3d7d000a71bba718",
+    "url": "/static/js/runtime-main.4159d1dc.js"
+  }
+]);
